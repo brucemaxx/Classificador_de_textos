71c6165004b0db41635977a7c343be0e71334c71
brucemaxx <silvabruce777@gmail.com>
2025-09-01 19:11:29 -0300

    Update utils.py

--- app/utils.py
+++ app/utils.py
@@ -4,3 +4,24 @@ import re
 import string
 import nltk
 from nltk.corpus import stopwords
+
+# Certifique-se de que as stopwords foram baixadas
+try:
+    nltk.data.find('corpora/stowords')
+except nltk.downloader.DownloadError:
+    nltk.download('stopwords')
+    
+stop_words = set(stopwords.words('portuguese'))  
+
+def preprocess_text(text: str) -> str:
+    """
+    Função para pré-processar o texto.
+    Remova URLs, menções, pontuação e stopwords.
+    """
+    
+    text = text.lower() # Converter para minúsculas
+    text = re.sub(r'http\S+|www\S+|https\S+', '', text, flags=re.MULTILINE) # Remove URLs
+    text = re.sub(r'@[A-Za-z0-9_]+', '', text) # Remove menções
+    text = text.translate(str.maketrans('', '', string.punctuation)) # Remove pontuação
+    text = ' '.join([word for word in text.split() if word not in stop_words]) # Remove stopwords
+    return text
